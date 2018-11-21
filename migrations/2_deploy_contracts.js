dad18e3f39f6afd1208e0dda29db17b4d8ccfbdf
Truls Asheim <truls@asheim.dk>
2018-11-21 19:26:08 +0100

    Cleanup old nest.js files

--- migrations/2_deploy_contracts.js
+++
@@ -1,9 +0,0 @@
-var EToroToken = artifacts.require("./EToroToken.sol");
-var EtoroRole = artifacts.require("./EToroRole.sol")
-
-module.exports = function(deployer) {
-   
-  deployer.deploy(EtoroRole).then((rolesContract) =>
-      deployer.deploy(EToroToken, "EToro USD", "ETUSD", 18, rolesContract.address)
-  );
-};
