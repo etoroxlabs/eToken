379f626427f33201efe4d23ada7194ec1a494aff
Truls Asheim <truls@asheim.dk>
2018-11-22 14:33:50 +0100

    Started migration towards new openzep API

--- contracts/NameRegistry.sol
+++ contracts/NameRegistry.sol
@@ -1,33 +1,48 @@
 pragma solidity ^0.4.24;
 
-import "./EToroRole.sol";
+import "openzeppelin-solidity/contracts/ownership/Ownable.sol";
 import "./KillSwitch.sol";
 
-contract NameRegistry is KillSwitch, EToroRole{
+contract NameRegistry is KillSwitch, Ownable {
 
     mapping(string => address) _registry;
 
-    function lookupName(string name) public view returns (address){
-        require(
-            _registry[name] != address(0), 
-            "Name does not exist."
-        );
+    function _nameExists(string name)
+      private
+      view
+      returns (bool) {
+      _registry[name] == address(0);
+    }
+
+    modifier nameExists(string name) {
+      require (_nameExists(name), "Name already exists");
+      _;
+    }
+
+    modifier nameNotExists(string name) {
+      require (!(_nameExists(name)), "Name does not exists");
+      _;
+    }
+
+    function lookupName(string name)
+      public nameExists
+      view
+      returns (address){
         return _registry[name];
     }
 
-    function createName(string name, address user) public onlyOwner {
-        require(
-            _registry[name] == address(0),
-            "Name already exists"
-        );
-        _registry[name] = user;
+    function createName(string name, address dest)
+      public onlyOwner nameNotExists {
+        _registry[name] = dest;
     }
 
-    function deleteName(string name) public onlyOwner {
-        require(
-            _registry[name] != address(0), 
-            "Name does not exist."
-        );
-        _registry[name] = address(0);
+    function modifyName(string oldName, string newName, address dest)
+      public onlyOwner nameExists {
+      _registry[name] = dest;
+    }
+
+    function deleteName(string name)
+      public onlyOwner nameExists {
+        delete _registry[name];
     }
 }
