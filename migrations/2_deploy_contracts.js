eebfc171e713933c60011a8e868d8757a38812b4
Mads <mads.da.mind@hotmail.com>
2018-11-27 14:54:38 +0100

    Added dependency to ENS and implemented deployment

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,7 +1,50 @@
 let Whitelist = artifacts.require('Whitelist');
 let TokenManager = artifacts.require('TokenManager');
 
+const ENS = artifacts.require('@ensdomains/ens/contracts/ENSRegistry.sol');
+const PublicResolver = artifacts.require('@ensdomains/ens/contracts/PublicResolver.sol');
+const ReverseRegistrar = artifacts.require('@ensdomains/ens/contracts/ReverseRegistrar.sol');
+const namehash = require('eth-ens-namehash');
+
 module.exports = function(deployer) {
-    deployer.deploy(Whitelist);
-    deployer.deploy(TokenManager);
+    if (deployer.network == 'development'){
+        deployer.deploy(Whitelist)
+        .then (() => {
+            deployer.deploy(TokenManager, Whitelist.address)
+            .then(() => {
+                return deployer.deploy(ENS)
+                .then(() => {
+                    return deployer.deploy(
+                        PublicResolver,
+                        ENS.address);
+                })
+                .then(() => {
+                    return deployer.deploy(
+                        ReverseRegistrar,
+                        ENS.address,
+                        PublicResolver.address);
+                })
+                .then(() => {
+                    return ENS.at(ENS.address)
+                    .setSubnodeOwner(
+                        0,
+                        web3.sha3(tld),
+                        owner, {from: owner});
+                })
+                .then(() => {
+                    return ENS.at(ENS.address)
+                    .setSubnodeOwner(0,
+                        web3.sha3('reverse'),
+                        owner,{from: owner});
+                })
+                .then(() => {
+                    return ENS.at(ENS.address)
+                    .setSubnodeOwner(
+                        namehash.hash('reverse'),
+                        web3.sha3('addr'),
+                        ReverseRegistrar.address, {from: owner});
+                })
+            })
+        })
+    }
 }
