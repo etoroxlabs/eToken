2eba98884af14cd12cbbd64dde453bdd97706707
Mads <mads.da.mind@hotmail.com>
2018-11-29 11:16:59 +0100

    Added setup accounts to truffle migrations flow

--- migrations/3_setup_accounts.js
+++ migrations/3_setup_accounts.js
@@ -2,18 +2,19 @@
 const Whitelist = artifacts.require("Whitelist");
 const TokenManager = artifacts.require("TokenManager");
 
-module.exports = function(callback) {
+module.exports = async function(deployer, _network, accounts) {
 
   /*
     The purpose of this is to automatically setup the test environment accounts.
     DO NOT use in production yet.
   */
 
-  const owner = web3.eth.accounts[0];
-  const whitelistAdmin = web3.eth.accounts[1];
-  const whitelisted = web3.eth.accounts[2];
+  if (deployer.network == 'development' ||
+      deployer.network == 'develop') {
 
-  const exec = async () => {
+    const owner = accounts[0];
+    const whitelistAdmin = accounts[1];
+    const whitelisted = accounts[2];
 
     // Setup whitelists
     const whitelistContract = await Whitelist.deployed();
@@ -27,9 +28,4 @@ module.exports = function(callback) {
     await tokenManagerContract.newToken("eToro US Dollar", "eUSD", 4, whitelistContract.address, { from: owner });
     await tokenManagerContract.newToken("eToro Australian Dollar", "eAUD", 4, whitelistContract.address, { from: owner });
   }
-
-  exec().then(
-    () => callback(),
-    (reason) => callback(reason)
-  )
 }
