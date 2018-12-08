e2b2ff6919b97c91d776e6a54199a388df528e27
Peter Emil Jensen <ubs908@gmail.com>
2018-12-08 16:20:11 +0100

    Added automatic linting fixes

--- migrations/3_setup_ens.js
+++ migrations/3_setup_ens.js
@@ -1,46 +1,44 @@
 
-const Whitelist = artifacts.require("Whitelist");
-const TokenManager = artifacts.require("TokenManager");
-const ENSRegistry = artifacts.require('ENSRegistry.sol');
-const PublicResolver = artifacts.require('PublicResolver.sol');
-const namehash = require('eth-ens-namehash');
+const Whitelist = artifacts.require('Whitelist')
+const TokenManager = artifacts.require('TokenManager')
+const ENSRegistry = artifacts.require('ENSRegistry.sol')
+const PublicResolver = artifacts.require('PublicResolver.sol')
+const namehash = require('eth-ens-namehash')
 
-async function setENS(name, parentNode, address, owner) {
-  const hashedname = namehash.hash(`${name}.${parentNode}`);
-  const ens = await ENSRegistry.deployed();
-  const resolver = await PublicResolver.deployed();
+async function setENS (name, parentNode, address, owner) {
+  const hashedname = namehash.hash(`${name}.${parentNode}`)
+  const ens = await ENSRegistry.deployed()
+  const resolver = await PublicResolver.deployed()
 
-  await ens.setSubnodeOwner(namehash.hash(parentNode), web3.sha3(name), owner, {from: owner});
-  await ens.setResolver(hashedname, PublicResolver.address, {from: owner});
-  
-  await resolver.setAddr(hashedname, address, {from: owner});
+  await ens.setSubnodeOwner(namehash.hash(parentNode), web3.sha3(name), owner, { from: owner })
+  await ens.setResolver(hashedname, PublicResolver.address, { from: owner })
+
+  await resolver.setAddr(hashedname, address, { from: owner })
 }
 
-async function getENSaddr(name) {
-  const hashedname = namehash.hash(name);
-  const resolver = await PublicResolver.deployed();
-  const res = await resolver.addr.call(hashedname);
-  
-  return res;
+async function getENSaddr (name) {
+  const hashedname = namehash.hash(name)
+  const resolver = await PublicResolver.deployed()
+  const res = await resolver.addr.call(hashedname)
+
+  return res
 }
 
-
-module.exports = (deployer, network, accounts) =>{
-  const owner = accounts[0];
-  const dummyaddress = '0x5aeda56215b167893e80b4fe645ba6d5bab767de'; //used accounts[9] as a placeholder for etokenize.eth addresss
-  const tld = 'eth';
+module.exports = (deployer, network, accounts) => {
+  const owner = accounts[0]
+  const dummyaddress = '0x5aeda56215b167893e80b4fe645ba6d5bab767de' // used accounts[9] as a placeholder for etokenize.eth addresss
+  const tld = 'eth'
   const etokenize_name = 'etokenize'
-  const etokenize_tld_name = etokenize_name + '.' + tld;
-  const whitelist_name = 'whitelist';
-  const tokenmanager_name = 'manager';
+  const etokenize_tld_name = etokenize_name + '.' + tld
+  const whitelist_name = 'whitelist'
+  const tokenmanager_name = 'manager'
 
   if (deployer.network == 'development' ||
       deployer.network == 'develop') {
-      deployer.then(async () => {
-      
-      await setENS(etokenize_name, tld, dummyaddress, owner);
-      await setENS(whitelist_name, etokenize_tld_name, Whitelist.address, owner);
-      await setENS(tokenmanager_name, etokenize_tld_name, TokenManager.address, owner);
-    });
+    deployer.then(async () => {
+      await setENS(etokenize_name, tld, dummyaddress, owner)
+      await setENS(whitelist_name, etokenize_tld_name, Whitelist.address, owner)
+      await setENS(tokenmanager_name, etokenize_tld_name, TokenManager.address, owner)
+    })
   }
 }
