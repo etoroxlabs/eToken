689a0e7af0c957c86fe64601abe0a4239d3e8c1f
Peter Emil Jensen <ubs908@gmail.com>
2018-12-09 21:14:27 +0100

    Moved setup accounts to migrations scripts (#28)
    * Moved setup_contracts to migrations
    
    * Coveralls are down so removing them from circleci test pass

---
+++ migrations/4_setup_accounts.js
@@ -0,0 +1,67 @@
+/* global artifacts, web3 */
+
+const Whitelist = artifacts.require('Whitelist')
+const TokenManager = artifacts.require('TokenManager')
+const EToroToken = artifacts.require('EToroToken')
+const ExternalERC20Storage = artifacts.require('ExternalERC20Storage')
+
+module.exports = function (deployer, _network, accounts) {
+
+  if (deployer.network === 'development' ||
+      deployer.network === 'develop') {
+    deployer.then(() => setupAccounts(accounts))
+  }
+}
+
+async function setupAccounts ([owner, whitelistAdmin, whitelisted, ...restAccounts]) {
+
+  /*
+    The purpose of this is to automatically setup the test environment accounts.
+    DO NOT use in production yet.
+  */
+
+  const intialMintValue = '100'
+
+  // Setup whitelists
+  const whitelistContract = await Whitelist.deployed()
+
+  await whitelistContract.addWhitelistAdmin(whitelistAdmin, { from: owner })
+  await whitelistContract.addWhitelisted(whitelisted, { from: whitelistAdmin })
+
+  // Setup tokens
+  const tokenManagerContract = await TokenManager.deployed()
+
+  const tokenDetails = [
+    {
+      name: 'eToro US Dollar',
+      symbol: 'eUSD',
+      decimals: 4
+    },
+    {
+      name: 'eToro Australian Dollar',
+      symbol: 'eAUD',
+      decimals: 4
+    }
+  ]
+
+  const tokens = await Promise.all(
+    tokenDetails.map(async (td) => {
+      const externalERC20Storage = await ExternalERC20Storage.new()
+      await tokenManagerContract.newToken(
+        td.name, td.symbol, td.decimals,
+        whitelistContract.address, externalERC20Storage.address,
+        { from: owner }
+      )
+
+      const tokenAddress = await tokenManagerContract.getToken(td.name, { from: owner })
+      await externalERC20Storage.transferImplementor(tokenAddress, { from: owner })
+
+      return EToroToken.at(tokenAddress)
+    })
+  )
+
+  // Mint tokens
+  await Promise.all(
+    tokens.map((t) => t.mint(owner, intialMintValue, { from: owner }))
+  )
+};
