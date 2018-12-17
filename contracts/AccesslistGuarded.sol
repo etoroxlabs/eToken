173d6493119df9a72d3a7d824a2ce30c53cc2bae
maasdamind <mads.da.mind@hotmail.com>
2018-12-17 12:02:05 +0100

    Added additional comments

--- contracts/AccesslistGuarded.sol
+++ contracts/AccesslistGuarded.sol
@@ -2,6 +2,11 @@ pragma solidity ^0.4.24;
 
 import "./Accesslist.sol";
 
+/**
+    @title The AccesslistGuarded contract
+    @dev Contract containing an accesslist and
+         modifiers to ensure proper access
+*/
 contract AccesslistGuarded {
 
     Accesslist private accesslist;
@@ -11,31 +16,58 @@ contract AccesslistGuarded {
         accesslist = _accesslist;
     }
 
+    /**
+        @dev Modifier that requires given address
+             to be whitelisted and not blacklisted
+        @param account address to be checked
+    */
     modifier requireHasAccess(address account) {
         require(accesslist.hasAccess(account));
         _;
     }
 
+    /**
+        @dev Modifier that requires the message sender
+             to be whitelisted and not blacklisted
+    */
     modifier onlyHasAccess() {
         require(accesslist.hasAccess(msg.sender));
         _;
     }
 
+    /**
+        @dev Modifier that requires given address
+             to be whitelisted
+        @param account address to be checked
+    */
     modifier requireWhitelisted(address account) {
         require(accesslist.isWhitelisted(account));
         _;
     }
 
+    /**
+        @dev Modifier that requires message sender
+             to be whitelisted
+    */
     modifier onlyWhitelisted() {
         require(accesslist.isWhitelisted(msg.sender));
         _;
     }
 
+    /**
+        @dev Modifier that requires given address
+             to not be blacklisted
+        @param account address to be checked
+    */
     modifier requireNotBlacklisted(address account) {
         require(!accesslist.isBlacklisted(account));
         _;
     }
 
+    /**
+        @dev Modifier that requires message sender
+             to not be blacklisted
+    */
     modifier onlyNotBlacklisted() {
         require(!accesslist.isBlacklisted(msg.sender));
         _;
