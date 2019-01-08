1a6215cfd442dd0f25a6cce9fa973462f54629e1
Truls Asheim <truls@asheim.dk>
2019-01-08 13:58:56 +0100

    Added event map for mintable

---
+++ test/token/ERC20/ExternalERC20Mintable.events.js
@@ -0,0 +1,8 @@
+const utils = require('./utils.js');
+
+module.exports = utils.makeEventMap({
+  changeMintingRecipient: (prevAddr, nextAddr) => [
+    { eventName: 'MintingRecipientAccountChanged',
+      paramMap: { prev: prevAddr,
+                  next: nextAddr } }]
+});
