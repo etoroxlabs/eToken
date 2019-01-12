343e5de85735ef50728b1810a23f50463fdac91c
Truls Asheim <truls@asheim.dk>
2019-01-13 00:27:31 +0100

    Update docs group1 (#60)
    * Added some comments
    
    * Group 1 comments
    
    * Fix minor inconsistencies
    
    * Typo

--- contracts/mocks/ExternalERC20PausableMock.sol
+++ contracts/mocks/ExternalERC20PausableMock.sol
@@ -4,9 +4,16 @@ import "../token/ERC20/ExternalERC20Pausable.sol";
 import "../token/ERC20/ExternalERC20Storage.sol";
 import "./PauserRoleMock.sol";
 
-// mock class using ERC20Pausable
+/**
+ * @title Mock contract for testing MinterRole
+ */
 contract ExternalERC20PausableMock is ExternalERC20Pausable, PauserRoleMock {
 
+    /** @dev Initializes an ERC20Pausable, sets up the external
+      * storage and mints an amount of token to the given account
+      * @param initialAccount The account that tokens should be minted to
+      * @param initialBalance The amount of tokens that should be minted
+     */
     constructor(address initialAccount, uint initialBalance)
         ExternalERC20(new ExternalERC20Storage())
         public
--- contracts/mocks/MinterRoleMock.sol
+++ contracts/mocks/MinterRoleMock.sol
@@ -2,19 +2,28 @@ pragma solidity ^0.4.24;
 
 import "../access/roles/MinterRole.sol";
 
+/**
+ * @title Mock contract for testing MinterRole
+ */
 contract MinterRoleMock is MinterRole {
 
+    /** Tests the msg.sender-dependent onlyMinter modifier */
     function onlyMinterMock() public view onlyMinter {
     }
 
+    /** Tests the requireMinter modifier which checks if the
+      * given address is a minter
+      * @param a The address to be checked
+     */
     function requireMinterMock(address a) public view requireMinter(a) {
     }
 
-    // Causes compilation errors if functions are not declared internal
+    /** Causes compilation errors if _removeMinter function is not declared internal */
     function _removeMinter(address account) internal {
         super._removeMinter(account);
     }
 
+    /** Causes compilation errors if _removeMinter function is not declared internal */
     function _addMinter(address account) internal {
         super._addMinter(account);
     }
