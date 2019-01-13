820a36d962ea7c61683932a522ebcd0b2a5ec8b2
Truls Asheim <truls@asheim.dk>
2019-01-13 01:37:44 +0100

    Fix actual and documented param name mismatch

--- contracts/mocks/TokenXMock.sol
+++ contracts/mocks/TokenXMock.sol
@@ -16,8 +16,8 @@ contract TokenXMock is TokenX, PauserRoleMock {
      * @param decimals The number of decimals of the token
      * @param accesslist Address of a deployed whitelist contract
      * @param whitelistEnabled Create token with whitelist enabled
-     * @param externalERC20Storage Address of a deployed ERC20 storage contract
-     * @param mintingRecipientAccount The initial minting recipient of the token
+     * @param stor Address of a deployed ERC20 storage contract
+     * @param mintingRecip The initial minting recipient for the token
      * @param upgradedFrom The token contract that this contract upgrades. Set
      * to address(0) for initial deployments
      * @param initialDeployment Set to true if this is the initial deployment of
