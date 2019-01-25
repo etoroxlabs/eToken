0305bed50da13c412f76ee9123e7338fe72e9ec9
Peter Emil Jensen <ubs908@gmail.com>
2019-01-25 19:53:41 +0100

    Locked the pragma version to 0.4.24 (#78)

--- contracts/mocks/ExternalERC20MintableMock.sol
+++ contracts/mocks/ExternalERC20MintableMock.sol
@@ -1,4 +1,4 @@
-pragma solidity ^0.4.24;
+pragma solidity 0.4.24;
 
 import "../token/ERC20/ExternalERC20Mintable.sol";
 import "./MinterRoleMock.sol";
--- contracts/mocks/TokenXMock.sol
+++ contracts/mocks/TokenXMock.sol
@@ -1,4 +1,4 @@
-pragma solidity ^0.4.24;
+pragma solidity 0.4.24;
 
 /* solium-disable max-len */
 import "../token/ERC20/ExternalERC20Storage.sol";
