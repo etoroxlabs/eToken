a2dc03343c960036dd603ef1914ad94aae4c3fcf
Truls Asheim <truls@asheim.dk>
2019-01-26 12:16:53 +0100

    Set initial implementor to pausable mock contract

--- test/token/ERC20/ExternalERC20Pausable.test.js
+++ test/token/ERC20/ExternalERC20Pausable.test.js
@@ -4,10 +4,14 @@
 const { shouldBehaveLikeERC20Pausable } = require('./behaviors/ERC20Pausable.behavior');
 
 const ExternalERC20PausableMock = artifacts.require('ExternalERC20PausableMock');
+const ExternalERC20Storage = artifacts.require('ExternalERC20Storage');
 
 contract('ExternalERC20Pausable', function ([_, pauser, otherPauser, recipient, anotherAccount, ...otherAccounts]) {
   beforeEach(async function () {
-    this.token = await ExternalERC20PausableMock.new(pauser, 100, { from: pauser });
+    const storage = await ExternalERC20Storage.new({ from: pauser });
+    this.token = await ExternalERC20PausableMock.new(pauser, 100,
+                                                     storage.address,
+                                                     { from: pauser });
   });
 
   shouldBehaveLikeERC20Pausable(pauser, otherPauser, recipient, anotherAccount, otherAccounts);
