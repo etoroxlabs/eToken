25c38ef61afd93584636c4f40f1ab17b5f2bb490
Truls Asheim <truls@asheim.dk>
2019-02-03 23:47:34 +0100

    Reenable gas reporter globally

--- truffle.js
+++ truffle.js
@@ -34,13 +34,12 @@ module.exports = {
       enabled: true,
       runs: 200
     }
+  },
+  mocha: {
+    reporter: 'eth-gas-reporter',
+    reporterOptions: {
+      currency: 'USD',
+      gasPrice: 20
+    }
   }
-  // ,
-  // mocha: {
-  //   reporter: 'eth-gas-reporter',
-  //   reporterOptions: {
-  //     currency: 'USD',
-  //     gasPrice: 20
-  //   }
-  // }
 };
