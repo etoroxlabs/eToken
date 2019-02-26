2f3a6ecd7def1287606c5c58031ca411de1bc9e5
Maasdamind <mads.da.mind@hotmail.com>
2019-02-26 18:43:46 +0100

    Mainnet Deployment (#98)

--- migrations/4_setup_accounts.js
+++ migrations/4_setup_accounts.js
@@ -9,19 +9,14 @@ const { ZERO_ADDRESS } = require('../test/utils.js');
 module.exports = function (deployer, _network, accounts) {
   if (deployer.network === 'development' ||
       deployer.network === 'develop' ||
-      deployer.network === 'ropsten') {
+      deployer.network === 'ropsten' ||
+      deployer.network === 'mainnet') {
     deployer.then(() => setupAccounts(accounts));
   }
 };
 
 async function setupAccounts ([owner, ..._]) {
-  /*
-    The purpose of this is to automatically setup the test environment accounts.
-    DO NOT use in production yet.
-  */
-
-  // FIXME: Use some real address here
-  const mintTargetAccount = 0xd00f;
+  const mintTargetAccount = owner;
 
   // Setup whitelists
   const accesslistContract = await Accesslist.deployed();
@@ -31,29 +26,81 @@ async function setupAccounts ([owner, ..._]) {
 
   const tokenDetails = [
     {
-      name: 'eToro Israel Shekel',
-      symbol: 'eILS',
-      decimals: 2,
-      whitelistEnabled: true
+      name: 'eToro New Zealand Dollar',
+      symbol: 'NZDX',
+      decimals: 18,
+      whitelistEnabled: false
     },
     {
-      name: 'eToro Danish krone',
-      symbol: 'eDKK',
-      decimals: 2,
+      name: 'eToro Japanese Yen',
+      symbol: 'JPYX',
+      decimals: 18,
+      whitelistEnabled: false
+    },
+    {
+      name: 'eToro Russian Ruble',
+      symbol: 'RUBX',
+      decimals: 18,
+      whitelistEnabled: false
+    },
+    {
+      name: 'eToro Chinese Yuan',
+      symbol: 'CNYX',
+      decimals: 18,
+      whitelistEnabled: false
+    },
+    {
+      name: 'eToro Swiss Franc',
+      symbol: 'CHFX',
+      decimals: 18,
+      whitelistEnabled: false
+    },
+    {
+      name: 'eToro United States Dollar',
+      symbol: 'USDX',
+      decimals: 18,
+      whitelistEnabled: false
+    },
+    {
+      name: 'eToro Euro',
+      symbol: 'EURX',
+      decimals: 18,
+      whitelistEnabled: false
+    },
+    {
+      name: 'eToro Pound sterling',
+      symbol: 'GBPX',
+      decimals: 18,
+      whitelistEnabled: false
+    },
+    {
+      name: 'eToro Australian Dollar',
+      symbol: 'AUDX',
+      decimals: 18,
+      whitelistEnabled: false
+    },
+    {
+      name: 'eToro Gold',
+      symbol: 'GLDX',
+      decimals: 18,
+      whitelistEnabled: false
+    },
+    {
+      name: 'eToro Silver',
+      symbol: 'SLVX',
+      decimals: 18,
       whitelistEnabled: false
     }
   ];
 
-  await Promise.all(
-    tokenDetails.map(async (td) => {
-      const token = await EToken.new(
-        td.name, td.symbol, td.decimals,
-        accesslistContract.address, td.whitelistEnabled,
-        ZERO_ADDRESS,
-        mintTargetAccount, ZERO_ADDRESS, true,
-        { from: owner });
+  for (const td of tokenDetails) {
+    const token = await EToken.new(
+      td.name, td.symbol, td.decimals,
+      accesslistContract.address, td.whitelistEnabled,
+      ZERO_ADDRESS,
+      mintTargetAccount, ZERO_ADDRESS, true,
+      { from: owner });
 
-      await tokenManagerContract.addToken(td.name, token.address);
-      return token;
-    }));
+    await tokenManagerContract.addToken(td.name, token.address);
+  }
 }
--- truffle.js
+++ truffle.js
@@ -12,8 +12,18 @@
  *   },
  */
 
+require('@babel/polyfill');
+
 const HDWalletProvider = require('truffle-hdwallet-provider');
+const LedgerWalletProvider = require('truffle-ledger-provider');
+
 const mnemonic = process.env.ETOKEN_MNEMONIC;
+const ledgerOptions = {
+  path: "44'/60'/0'/0/0",
+  askConfirm: true,
+  accountsLength: 1,
+  accountsOffset: 0
+};
 
 module.exports = {
   networks: {
@@ -25,11 +35,19 @@ module.exports = {
     },
     ropsten: {
       provider: function () {
-        return new HDWalletProvider(mnemonic, 'https://ropsten.infura.io/v3/98b6008ef68a4389a5b10240f197e650');
+        return new HDWalletProvider(mnemonic, 'https://ropsten.infura.io/');
       },
       gas: 8000000,
       gasPrice: 1000000,
       network_id: 3
+    },
+    mainnet: {
+      provider: function () {
+        return new LedgerWalletProvider(ledgerOptions, 'https://mainnet.infura.io/');
+      },
+      gas: 8000000,
+      gasPrice: 60000000000,
+      network_id: 1
     },
     coverage: {
       host: '127.0.0.1',
