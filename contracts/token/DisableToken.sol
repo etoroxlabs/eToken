58a6e0075d82a707e4447999c91c4c9d6b9b1459
Peter Emil Jensen <ubs908@gmail.com>
2019-03-05 11:04:00 +0100

    Added script for disabling tokens (#101)
    * Added support for crippling tokens
    
    * Added test for DisableToken's upgrade method

---
+++ contracts/token/DisableToken.sol
@@ -0,0 +1,196 @@
+pragma solidity ^0.4.24;
+
+import "./IETokenProxy.sol";
+
+/**
+ * @title A token that doesn't work
+ * @dev Implements a proxy interface which always reverts. Used for
+ * testing if upgraded tokens actually forwards calls to the proxy.
+ */
+contract DisableToken is IETokenProxy {
+
+    /* solium-disable zeppelin/missing-natspec-comments */
+
+    function upgrade(IETokenProxy upgradedToken) public pure {
+        // Silence warnings
+        upgradedToken;
+        revert("Token is disabled");
+    }
+
+    function finalizeUpgrade() public {
+    }
+
+    function nameProxy(address sender)
+        public
+        view
+        returns(string)
+    {
+        sender;
+        return "DO NOT USE - Disabled";
+    }
+
+    function symbolProxy(address sender)
+        public
+        view
+        returns(string)
+    {
+        sender;
+        return "DEAD";
+    }
+
+    function decimalsProxy(address sender)
+        public
+        view
+        returns(uint8)
+    {
+        // Silence warnings
+        sender;
+        return 0;
+    }
+
+    function totalSupplyProxy(address sender)
+        public
+        view
+        returns(uint256)
+    {
+        // Silence warnings
+        sender;
+        return 0;
+    }
+
+    function balanceOfProxy(address sender, address who)
+        public
+        view
+        returns(uint256)
+    {
+        // Silence warnings
+        sender;
+        who;
+        return 0;
+    }
+
+    function allowanceProxy(address sender, address owner, address spender)
+        public
+        view
+        returns(uint256)
+    {
+        // Silence warnings
+        sender;
+        owner;
+        spender;
+        return 0;
+    }
+
+    function transferProxy(address sender, address to, uint256 value)
+        public
+        returns (bool)
+    {
+        sender;
+        to;
+        value;
+        revert("Token is disabled");
+    }
+
+    function approveProxy(address sender, address spender, uint256 value)
+        public
+        returns (bool)
+    {
+        sender;
+        spender;
+        value;
+        revert("Token is disabled");
+    }
+
+    function transferFromProxy(
+        address sender,
+        address from,
+        address to,
+        uint256 value
+    )
+        public
+        returns (bool)
+    {
+        sender;
+        from;
+        to;
+        value;
+        revert("Token is disabled");
+    }
+
+    function increaseAllowanceProxy(
+        address sender,
+        address spender,
+        uint256 addedValue
+    )
+        public
+        returns (bool)
+    {
+        sender;
+        spender;
+        addedValue;
+        revert("Token is disabled");
+    }
+
+    function decreaseAllowanceProxy(address sender,
+                                    address spender,
+                                    uint256 subtractedValue)
+        public
+        returns (bool)  {
+        sender;
+        spender;
+        subtractedValue;
+        revert("Token is disabled");
+    }
+
+    function burnProxy(address sender, uint256 value)
+        public
+    {
+        sender;
+        value;
+        revert("Token is disabled");
+    }
+
+    function burnFromProxy(address sender,
+                           address from,
+                           uint256 value)
+        public
+    {
+        sender;
+        from;
+        value;
+        revert("Token is disabled");
+    }
+
+    function mintProxy(address sender, address to, uint256 value)
+        public
+        returns (bool)
+    {
+        sender;
+        to;
+        value;
+        revert("Token is disabled");
+    }
+
+    function changeMintingRecipientProxy(address sender, address mintingRecip)
+        public
+    {
+        sender;
+        mintingRecip;
+        revert("Token is disabled");
+    }
+
+    function pauseProxy(address sender) external {
+        sender;
+        revert("Token is disabled");
+    }
+
+    function unpauseProxy(address sender) external {
+        sender;
+        revert("Token is disabled");
+    }
+
+    function pausedProxy(address sender) external view returns (bool) {
+        sender;
+        revert("Token is disabled");
+    }
+}
---
+++ test/DisableToken.test.js
@@ -0,0 +1,70 @@
+/* global artifacts, web3, contract */
+/* eslint-env mocha */
+
+'use strict';
+
+const Accesslist = artifacts.require('Accesslist');
+const ETokenMock = artifacts.require('ETokenMock');
+const DisableToken = artifacts.require('DisableToken');
+
+const utils = require('./utils.js');
+const disableTokenAction = require('../scripts/disableToken/disableToken.js');
+
+const BigNumber = web3.BigNumber;
+
+require('chai')
+  .use(require('chai-bignumber')(BigNumber))
+  .should();
+
+contract('Disable Token', async function ([_, owner, otherAccount]) {
+  it('Should properly disable token',
+     async function () {
+       const tokenName = 'test';
+       const tokenSymbol = 'test';
+       const tokenDecimals = 4;
+
+       const accesslist = await Accesslist.new();
+
+       const token = await ETokenMock.new(
+         tokenName, tokenSymbol, tokenDecimals,
+         accesslist.address, false, utils.ZERO_ADDRESS, owner, 0, true,
+         owner, 10000,
+         { from: owner }
+       );
+
+       await token.approve(otherAccount, 100, { from: owner });
+
+       (await token.name()).should.be.equal(tokenName);
+       (await token.symbol()).should.be.equal(tokenSymbol);
+       (await token.decimals()).should.be.bignumber.equal(tokenDecimals);
+       (await token.totalSupply()).should.be.bignumber.equal(10000);
+       (await token.balanceOf(owner)).should.be.bignumber.equal(10000);
+       (await token.allowance(owner, otherAccount)).should.be.bignumber.equal(100);
+
+       await disableTokenAction(DisableToken, token, owner);
+
+       (await token.name()).should.be.equal('DO NOT USE - Disabled');
+       (await token.symbol()).should.be.equal('DEAD');
+       (await token.decimals()).should.be.bignumber.equal(0);
+       (await token.totalSupply()).should.be.bignumber.equal(0);
+       (await token.balanceOf(owner)).should.be.bignumber.equal(0);
+       (await token.allowance(owner, otherAccount)).should.be.bignumber.equal(0);
+
+       const crippledText = 'Token is disabled';
+       await utils.assertRevertsReason(token.transfer(owner, 100), crippledText);
+       await utils.assertRevertsReason(token.approve(owner, 100), crippledText);
+       await utils.assertRevertsReason(token.transferFrom(owner, owner, 100), crippledText);
+       await utils.assertRevertsReason(token.increaseAllowance(owner, 100), crippledText);
+       await utils.assertRevertsReason(token.decreaseAllowance(owner, 100), crippledText);
+       await utils.assertRevertsReason(token.burn(100), crippledText);
+       await utils.assertRevertsReason(token.burnFrom(owner, 100), crippledText);
+       await utils.assertRevertsReason(token.mint(owner, 100), crippledText);
+       await utils.assertRevertsReason(token.changeMintingRecipient(owner), crippledText);
+       await utils.assertRevertsReason(token.pause(), crippledText);
+       await utils.assertRevertsReason(token.unpause(), crippledText);
+       await utils.assertRevertsReason(token.paused(), crippledText);
+
+       const disabledToken = DisableToken.at(await token.getUpgradedToken());
+       await utils.assertRevertsReason(disabledToken.upgrade(utils.ZERO_ADDRESS), crippledText);
+     });
+});
